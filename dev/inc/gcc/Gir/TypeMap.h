c4e1fb063086c50999065504b4a78563ff2b844d
Raja Mukherji <rajamukherji@gmail.com>
2017-04-19 07:27:30 +0100

    Add missing TypeMap files.

---
+++ dev/inc/gcc/Gir/TypeMap.h
@@ -0,0 +1,13 @@
+#ifndef GIR_TYPEMAP_H
+#define GIR_TYPEMAP_H
+
+#include <Agg/StringTable.h>
+
+#define RIVA_MODULE Gir$TypeMap
+#include <Riva-Header.h>
+
+extern Agg$StringTable_t Gir$TypeMap$Table[];
+
+#undef RIVA_MODULE
+
+#endif
