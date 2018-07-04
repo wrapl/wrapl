edf79d8f0d7ba270fe4b26990833f83756aa1f9a
Raja Mukherji <rajamukherji@gmail.com>
2018-07-04 08:20:59 +0100

    Added a few functions to Html.Gumbo preparing for Gtk doc extraction.

--- dev/src/lib/Html/Gumbo.c
+++ dev/src/lib/Html/Gumbo.c
@@ -26,8 +26,8 @@ TYPE(ElementNodeT, NodeT);
 TYPE(TextNodeT, NodeT);
 TYPE(CDataNodeT, TextNodeT, NodeT);
 TYPE(CommentNodeT, TextNodeT, NodeT);
-TYPE(WhitespaceNodeT, NodeT);
-TYPE(TemplateNodeT, NodeT);
+TYPE(WhitespaceNodeT, TextNodeT, NodeT);
+TYPE(TemplateNodeT, ElementNodeT, NodeT);
 
 TYPE(VectorT);
 
@@ -210,6 +210,70 @@ METHOD("[]", TYP, VectorT, TYP, Std$Integer$SmallT) {
 	return SUCCESS;
 }
 
+typedef struct vector_values_generator {
+	Std$Function_cstate State;
+	GumboVector *Vector;
+	int Index;
+} vector_values_generator;
+
+typedef struct vector_values_resume_data {
+	vector_values_generator *Generator;
+	Std$Function_argument Result;
+} vector_values_resume_data;
+
+static Std$Function$status resume_vector_values(vector_values_resume_data *Data) {
+	int Index = Data->Generator->Index;
+	gumbo_node_t *Node = new(gumbo_node_t);
+	Node->Handle = Data->Generator->Vector->data[Index];
+	switch (Node->Handle->type) {
+	case GUMBO_NODE_DOCUMENT: Node->Type = DocumentNodeT; break;
+	case GUMBO_NODE_ELEMENT: Node->Type = ElementNodeT; break;
+	case GUMBO_NODE_TEXT: Node->Type = TextNodeT; break;
+	case GUMBO_NODE_CDATA: Node->Type = CDataNodeT; break;
+	case GUMBO_NODE_COMMENT: Node->Type = CommentNodeT; break;
+	case GUMBO_NODE_WHITESPACE: Node->Type = WhitespaceNodeT; break;
+	case GUMBO_NODE_TEMPLATE: Node->Type = TemplateNodeT; break;
+	default: Node->Type = NodeT; break;
+	}
+	++Index;
+	Data->Result.Val = (Std$Object$t *)Node;
+	Data->Result.Ref = 0;
+	if (Index < Data->Generator->Vector->length) {
+		Data->Generator->Index = Index;
+		return SUSPEND;
+	} else {
+		return SUCCESS;
+	}
+}
+
+METHOD("values", TYP, VectorT) {
+//@vector
+//:ANY
+	gumbo_vector_t *Vector = (gumbo_vector_t *)Args[0].Val;
+	if (Vector->Handle->length == 0) return FAILURE;
+	gumbo_node_t *Node = new(gumbo_node_t);
+	Node->Handle = Vector->Handle->data[0];
+	switch (Node->Handle->type) {
+	case GUMBO_NODE_DOCUMENT: Node->Type = DocumentNodeT; break;
+	case GUMBO_NODE_ELEMENT: Node->Type = ElementNodeT; break;
+	case GUMBO_NODE_TEXT: Node->Type = TextNodeT; break;
+	case GUMBO_NODE_CDATA: Node->Type = CDataNodeT; break;
+	case GUMBO_NODE_COMMENT: Node->Type = CommentNodeT; break;
+	case GUMBO_NODE_WHITESPACE: Node->Type = WhitespaceNodeT; break;
+	case GUMBO_NODE_TEMPLATE: Node->Type = TemplateNodeT; break;
+	default: Node->Type = NodeT; break;
+	}
+	Result->Val = (Std$Object$t *)Node;
+	if (Vector->Handle->length == 1) return SUCCESS;
+	vector_values_generator *Generator = new(vector_values_generator);
+	Generator->State.Run = Std$Function$resume_c;
+	Generator->State.Invoke = (Std$Function_cresumefn)resume_vector_values;
+	Generator->Index = 1;
+	Generator->Vector = Vector->Handle;
+	Result->State = Generator;
+	return SUSPEND;
+}
+
 METHOD("[]", TYP, ElementNodeT, TYP, Std$Integer$SmallT) {
 	gumbo_node_t *Parent = (gumbo_node_t *)Args[0].Val;
 	int Index = Std$Integer$get_small(Args[1].Val);
@@ -229,6 +293,148 @@ METHOD("[]", TYP, ElementNodeT, TYP, Std$Integer$SmallT) {
 	}
 	Result->Val = (Std$Object$t *)Node;
 	return SUCCESS;
+}
+
+METHOD("values", TYP, ElementNodeT) {
+//@vector
+//:ANY
+	gumbo_node_t *Element = (gumbo_node_t *)Args[0].Val;
+	if (Element->Handle->v.element.children.length == 0) return FAILURE;
+	gumbo_node_t *Node = new(gumbo_node_t);
+	Node->Handle = Element->Handle->v.element.children.data[0];
+	switch (Node->Handle->type) {
+	case GUMBO_NODE_DOCUMENT: Node->Type = DocumentNodeT; break;
+	case GUMBO_NODE_ELEMENT: Node->Type = ElementNodeT; break;
+	case GUMBO_NODE_TEXT: Node->Type = TextNodeT; break;
+	case GUMBO_NODE_CDATA: Node->Type = CDataNodeT; break;
+	case GUMBO_NODE_COMMENT: Node->Type = CommentNodeT; break;
+	case GUMBO_NODE_WHITESPACE: Node->Type = WhitespaceNodeT; break;
+	case GUMBO_NODE_TEMPLATE: Node->Type = TemplateNodeT; break;
+	default: Node->Type = NodeT; break;
+	}
+	Result->Val = (Std$Object$t *)Node;
+	if (Element->Handle->v.element.children.length == 1) return SUCCESS;
+	vector_values_generator *Generator = new(vector_values_generator);
+	Generator->State.Run = Std$Function$resume_c;
+	Generator->State.Invoke = (Std$Function_cresumefn)resume_vector_values;
+	Generator->Index = 1;
+	Generator->Vector = &Element->Handle->v.element.children;
+	Result->State = Generator;
+	return SUSPEND;
+}
+
+typedef struct find_node_t find_node_t;
+
+struct find_node_t {
+	find_node_t *Next;
+	GumboVector *Vector;
+	int Index;
+};
+
+typedef struct element_find_generator {
+	Std$Function_cstate State;
+	find_node_t *FindNode;
+	Std$Object$t *Filter;
+} element_find_generator;
+
+static Std$Function$status resume_element_find(Std$Function$cresumedata *Data) {
+	element_find_generator *Generator = (element_find_generator *)Data->State;
+	find_node_t *FindNode = Generator->FindNode;
+	for (;;) {
+		if (!FindNode) return FAILURE;
+		int Index = FindNode->Index;
+		gumbo_node_t *Node = new(gumbo_node_t);
+		Node->Handle = FindNode->Vector->data[Index];
+		if (++Index == FindNode->Vector->length) {
+			FindNode = FindNode->Next;
+		} else {
+			FindNode->Index = Index;
+		}
+		switch (Node->Handle->type) {
+		case GUMBO_NODE_DOCUMENT: Node->Type = DocumentNodeT; break;
+		case GUMBO_NODE_ELEMENT: {
+			Node->Type = ElementNodeT;
+			if (Node->Handle->v.element.children.length) {
+				find_node_t *NewNode = new(find_node_t);
+				NewNode->Next = FindNode;
+				NewNode->Index = 0;
+				NewNode->Vector = &Node->Handle->v.element.children;
+				FindNode = NewNode;
+			}
+			break;
+		}
+		case GUMBO_NODE_TEXT: Node->Type = TextNodeT; break;
+		case GUMBO_NODE_CDATA: Node->Type = CDataNodeT; break;
+		case GUMBO_NODE_COMMENT: Node->Type = CommentNodeT; break;
+		case GUMBO_NODE_WHITESPACE: Node->Type = WhitespaceNodeT; break;
+		case GUMBO_NODE_TEMPLATE: Node->Type = TemplateNodeT; break;
+		default: Node->Type = NodeT; break;
+		}
+		switch (Std$Function$call(Generator->Filter, 1, &Data->Result, Node, 0)) {
+		case SUSPEND: case SUCCESS:
+			Generator->FindNode = FindNode;
+			return SUSPEND;
+		case FAILURE:
+			break;
+		case MESSAGE:
+			return MESSAGE;
+		}
+	}
+}
+
+METHOD("find", TYP, ElementNodeT, ANY) {
+	gumbo_node_t *Element = (gumbo_node_t *)Args[0].Val;
+	if (Element->Handle->v.element.children.length == 0) return FAILURE;
+	find_node_t *FindNode = new(find_node_t);
+	FindNode->Index = 0;
+	FindNode->Vector = &Element->Handle->v.element.children;
+	for (;;) {
+		if (!FindNode) return FAILURE;
+		int Index = FindNode->Index;
+		gumbo_node_t *Node = new(gumbo_node_t);
+		Node->Handle = FindNode->Vector->data[Index];
+		if (++Index == FindNode->Vector->length) {
+			FindNode = FindNode->Next;
+		} else {
+			FindNode->Index = Index;
+		}
+		switch (Node->Handle->type) {
+		case GUMBO_NODE_DOCUMENT: Node->Type = DocumentNodeT; break;
+		case GUMBO_NODE_ELEMENT: {
+			Node->Type = ElementNodeT;
+			if (Node->Handle->v.element.children.length) {
+				find_node_t *NewNode = new(find_node_t);
+				NewNode->Next = FindNode;
+				NewNode->Index = 0;
+				NewNode->Vector = &Node->Handle->v.element.children;
+				FindNode = NewNode;
+			}
+			break;
+		}
+		case GUMBO_NODE_TEXT: Node->Type = TextNodeT; break;
+		case GUMBO_NODE_CDATA: Node->Type = CDataNodeT; break;
+		case GUMBO_NODE_COMMENT: Node->Type = CommentNodeT; break;
+		case GUMBO_NODE_WHITESPACE: Node->Type = WhitespaceNodeT; break;
+		case GUMBO_NODE_TEMPLATE: Node->Type = TemplateNodeT; break;
+		default: Node->Type = NodeT; break;
+		}
+		switch (Std$Function$call(Args[1].Val, 1, Result, Node, 0)) {
+		case SUSPEND: case SUCCESS: {
+			element_find_generator *Generator = new(element_find_generator);
+			Generator->State.Run = Std$Function$resume_c;
+			Generator->State.Invoke = (Std$Function_cresumefn)resume_element_find;
+			Generator->FindNode = FindNode;
+			Generator->Filter = Args[1].Val;
+			Result->State = Generator;
+			return SUSPEND;
+		}
+		case FAILURE:
+			break;
+		case MESSAGE:
+			return MESSAGE;
+		}
+
+	}
 }
 
 METHOD("[]", TYP, DocumentNodeT, TYP, Std$Integer$SmallT) {
