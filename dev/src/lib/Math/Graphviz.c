8607f3412ab9778837a727ffbd8fc4b82b4bfc49
Raja Mukherji <rajamukherji@gmail.com>
2018-12-28 08:21:19 +0000

    Updates.

--- dev/src/lib/Math/Graphviz.c
+++ dev/src/lib/Math/Graphviz.c
@@ -1,6 +1,8 @@
 #include <Riva/Memory.h>
 #include <Std.h>
+#include <Agg/List.h>
 #include <graphviz/gvc.h>
+#include <graphviz/cgraph.h>
 #include <pthread.h>
 
 static GVC_t *Context;
@@ -36,15 +38,34 @@ TYPE(EdgeT, ObjectT);
 
 METHOD("get", TYP, ObjectT, TYP, Std$String$T) {
 	object_t *Object = (object_t *)Args[0].Val;
-	Result->Val = Std$String$new(agget(Object->Handle, Std$String$flatten(Args[1].Val)));
-	return SUCCESS;
-};
+	char *Value = agget(Object->Handle, Std$String$flatten(Args[1].Val));
+	if (Value) {
+		RETURN(Std$String$new(Value));
+	} else {
+		FAIL;
+	}
+}
+
+METHOD(".", TYP, ObjectT, TYP, Std$String$T) {
+	object_t *Object = (object_t *)Args[0].Val;
+	char *Value = agget(Object->Handle, Std$String$flatten(Args[1].Val));
+	if (Value) {
+		RETURN(Std$String$new(Value));
+	} else {
+		FAIL;
+	}
+}
 
 METHOD("set", TYP, ObjectT, TYP, Std$String$T, TYP, Std$String$T) {
 	object_t *Object = (object_t *)Args[0].Val;
 	agsafeset(Object->Handle, Std$String$flatten(Args[1].Val), Std$String$flatten(Args[2].Val), Std$String$flatten(Args[2].Val));
 	return SUCCESS;
-};
+}
+
+METHOD("@", TYP, ObjectT, VAL, Std$String$T) {
+	object_t *Object = (object_t *)Args[0].Val;
+	RETURN(Std$String$new(agnameof(Object->Handle)));
+}
 
 typedef struct type_t {
 	const Std$Type$t *Type;
@@ -68,48 +89,124 @@ GLOBAL_FUNCTION(New, 2) {
 	return SUCCESS;
 };
 
-METHOD("node", TYP, GraphT, TYP, Std$String$T) {
+GLOBAL_FUNCTION(Plugins, 1) {
+	CHECK_EXACT_ARG_TYPE(0, Std$String$T);
+	int Size;
+	const char **Names = gvPluginList(Context, Std$String$flatten(Args[0].Val), &Size, "");
+	Std$Object$t *List = Agg$List$new0();
+	for (int I = 0; I < Size; ++I) {
+		Agg$List$put(List, Std$String$new(Names[I]));
+	}
+	RETURN(List);
+}
+
+METHOD("node", TYP, GraphT) {
 	_graph_t *Graph = (_graph_t *)Args[0].Val;
 	_node_t *Node = new(_node_t);
+	char *Name;
+	if (Count > 1) {
+		Name = Std$String$flatten(Args[1].Val);
+	} else {
+		asprintf(&Name, "N%d", agnnodes(Graph->Handle));
+	}
 	Node->Type = NodeT;
-	Node->Handle = agnode(Graph->Handle, Std$String$flatten(Args[1].Val), 1);
-	Result->Val = (Std$Object$t *)Node;
-	return SUCCESS;
+	Node->Handle = agnode(Graph->Handle, Name, 1);
+	RETURN(Node);
 };
+
+METHOD("nodes", TYP, GraphT) {
+	_graph_t *Graph = (_graph_t *)Args[0].Val;
+	Std$Object$t *Nodes = Agg$List$new0();
+	Agnode_t *N = agfstnode(Graph->Handle);
+	while (N) {
+		_node_t *Node = new(_node_t);
+		Node->Type = NodeT;
+		Node->Handle = N;
+		Agg$List$put(Nodes, Node);
+		N = agnxtnode(Graph->Handle, N);
+	}
+	RETURN(Nodes);
+}
 
 METHOD("edge", TYP, GraphT, TYP, NodeT, TYP, NodeT) {
 	_graph_t *Graph = (_graph_t *)Args[0].Val;
+	_node_t *Head = (_node_t *)Args[1].Val;
+	_node_t *Tail = (_node_t *)Args[2].Val;
 	_edge_t *Edge = new(_edge_t);
 	char *Name;
 	if (Count > 3) {
 		Name = Std$String$flatten(Args[3].Val);
 	} else {
-		Name = asprintf("E%d", agnedges(Graph->Handle));
-	};
+		asprintf(&Name, "%s->%s", agnameof(Head->Handle), agnameof(Tail->Handle));
+	}
 	Edge->Type = EdgeT;
-	Edge->Handle = agedge(Graph->Handle,
-		((_node_t *)Args[2].Val)->Handle,
-		((_node_t *)Args[1].Val)->Handle,
-		Name,
-		1
-	);
-	Result->Val = (Std$Object$t *)Edge;
-	return SUCCESS;
-};
+	Edge->Handle = agedge(Graph->Handle, Tail->Handle, Head->Handle, Name, 1);
+	RETURN(Edge);
+}
 
-METHOD("render", TYP, GraphT, TYP, Std$String$T) {
+METHOD("edges", TYP, GraphT, TYP, NodeT) {
+	_graph_t *Graph = (_graph_t *)Args[0].Val;
+	_node_t *Node = (_node_t *)Args[1].Val;
+	Std$Object$t *Edges = Agg$List$new0();
+	Agedge_t *E = agfstedge(Graph->Handle, Node->Handle);
+	while (E) {
+		_edge_t *Edge = new(_edge_t);
+		Edge->Type = EdgeT;
+		Edge->Handle = E;
+		Agg$List$put(Edges, Edge);
+		E = agnxtedge(Graph->Handle, E, Node->Handle);
+	}
+	RETURN(Edges);
+}
+
+METHOD("subgraph", TYP, GraphT) {
+	_graph_t *Graph = (_graph_t *)Args[0].Val;
+	_graph_t *SubGraph = new(_graph_t);
+	char *Name;
+	if (Count > 1) {
+		Name = Std$String$flatten(Args[1].Val);
+	} else {
+		asprintf(&Name, "G%d", agnsubg(Graph->Handle));
+	}
+	SubGraph->Type = GraphT;
+	SubGraph->Handle = agsubg(Graph->Handle, Name, 1);
+	RETURN(SubGraph);
+}
+
+METHOD("delete", TYP, GraphT, TYP, ObjectT) {
+	_graph_t *Graph = (_graph_t *)Args[0].Val;
+	object_t *Object = (object_t *)Args[1].Val;
+	agdelete(Graph->Handle, Object->Handle);
+	RETURN0;
+}
+
+METHOD("render", TYP, GraphT, TYP, Std$String$T, TYP, Std$String$T) {
 	_graph_t *Graph = (_graph_t *)Args[0].Val;
 	const char *Layout = Std$String$flatten(Args[1].Val);
+	const char *Render = Std$String$flatten(Args[2].Val);
 	pthread_mutex_lock(Mutex);
 	gvLayout(Context, Graph->Handle, Layout);
-	gvRender(Context, Graph->Handle, "dot", stdout);
+	attach_attrs(Graph->Handle);
+	char *Data;
+	int Length;
+	gvRenderData(Context, Graph->Handle, Render, &Data, &Length);
 	gvFreeLayout(Context, Graph->Handle);
 	pthread_mutex_unlock(Mutex);
-	Result->Arg = Args[0];
-	return SUCCESS;
-};
+	RETURN(Std$String$new_length(Data, Length));
+}
+
+extern gvplugin_library_t gvplugin_dot_layout_LTX_library;
+extern gvplugin_library_t gvplugin_neato_layout_LTX_library;
+extern gvplugin_library_t gvplugin_core_LTX_library;
+extern gvplugin_library_t gvplugin_pango_LTX_library;
+extern gvplugin_library_t gvplugin_rsvg_LTX_library;
 
 INITIAL() {
 	//aginit();
 	Context = gvContext();
-};
+	gvAddLibrary(Context, &gvplugin_dot_layout_LTX_library);
+	gvAddLibrary(Context, &gvplugin_neato_layout_LTX_library);
+	gvAddLibrary(Context, &gvplugin_core_LTX_library);
+	gvAddLibrary(Context, &gvplugin_pango_LTX_library);
+	gvAddLibrary(Context, &gvplugin_rsvg_LTX_library);
+}
