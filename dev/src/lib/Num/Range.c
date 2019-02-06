99436424868e5d96b5e639f84e076b5b2d313963
Raja Mukherji <rajamukherji@gmail.com>
2019-02-06 07:32:04 +0000

    Updates.

---
+++ dev/src/lib/Num/Range.c
@@ -0,0 +1,32 @@
+#include <Std.h>
+#include <Riva.h>
+#include <Agg/List.h>
+#include <Num/Range.h>
+
+TYPE(T);
+
+Num$Range$t *_new(int Min, int Max, int Step) {
+	Num$Range$t *Range = new(Num$Range$t);
+	Range->Type = T;
+	Range->Min = Min;
+	Range->Max = Max;
+	Range->Step = Step;
+	return Range;
+}
+
+GLOBAL_FUNCTION(New, 3) {
+	CHECK_EXACT_ARG_TYPE(0, Std$Integer$SmallT);
+	CHECK_EXACT_ARG_TYPE(1, Std$Integer$SmallT);
+	CHECK_EXACT_ARG_TYPE(2, Std$Integer$SmallT);
+	Num$Range$t *Range = new(Num$Range$t);
+	Range->Type = T;
+	Range->Min = Std$Integer$get_small(Args[0].Val);
+	Range->Max = Std$Integer$get_small(Args[1].Val);
+	Range->Step = Std$Integer$get_small(Args[2].Val);
+	RETURN(Range);
+}
+
+METHOD("@", TYP, T, VAL, Std$String$T) {
+	Num$Range$t *Range = (Num$Range$t *)Args[0].Val;
+	RETURN(Std$String$new_format("%d, %d, ... %d", Range->Min, Range->Min + Range->Step, Range->Max));
+}
